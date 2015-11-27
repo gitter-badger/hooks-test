b94cff8401fd506fbaf431697743c67f80bea34b
chico <chi187@gmail.com>
2015-11-27 22:46:02 +0300

    hubh

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 
 app.post('/', req => {
   console.log(req.body);
 });
 
-app.listen(3000);
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
