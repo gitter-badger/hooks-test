25f0e471edbff5fff7fb3344c40e886ad51208ef
chico <chi187@gmail.com>
2015-11-27 23:43:23 +0300

    a

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const githubMiddleware = require('github-webhook-middleware')({
+  secret: process.env.GITHUB_SECRET,
+});
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(githubMiddleware);
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
+  if (req.get('X-Hub-Signature'))
   console.log(req.body);
   res.end();
 });
